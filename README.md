bbcf16e09a0317bffb4f349fc900fa19346aef90
Lidiya <l.glazikova@gmail.com>
2025-04-29 17:46:43 +0300

    create project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # qa-js-project-2025
 Пет-проект тестирования API и UI
+
+
+Для E2E тестирования загружаем в проект `Playwright` командой  
+
+```
+npm init playwright@latest
+```
---
+++ tests/metro.spec.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://qa-metro.stand-2.praktikum-services.ru/moscow');
+
+  await page.locator('.lang-switcher__selected-item').click(); 
+
+  await page.locator('._lang-code_en').click();
+
+  const lang = await page.locator('html').getAttribute('lang');
+  
+  // Проверяем, что атрибут lang содержит 'en'
+  await expect(page).toHaveURL(/.*\?lang=en$/);
+  
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
